014acb3a1d44efe6250dcec2db45986d0e87e3cf
amenprog <amenprog@gmail.com>
2018-10-24 18:24:27 +0300

    adding http.js

---
+++ http/http.js
@@ -0,0 +1,37 @@
+(function() {
+  var h = {};
+
+  h.get = function(url, callback) {
+    fetch(url, {
+      method: "GET",
+      credentials: "include"
+    })
+      .then(response => response.json())
+      .then(json => {
+        if (callback) callback(json);
+        else console.log(json);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  h.post = function(url, payload, callback) {
+    fetch(url, {
+      method: "POST", // or 'PUT'
+      body: JSON.stringify(payload), // data can be `string` or {object}!
+      headers: {
+        "Content-Type": "application/json"
+      },
+      credentials: "include"
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (callback) callback(data);
+        else console.log(data);
+      })
+      .catch(error => console.error(error));
+  };
+
+  window.h = h;
+})();
